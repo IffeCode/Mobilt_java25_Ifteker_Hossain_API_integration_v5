cbf19ee310d4987b800d755772a8509eca0ba539
iffe <iffe.h@hotmail.com>
2026-09-10 01:20:07 +0200

    Login + Registration complete

--- app/src/main/java/com/iffecode/api_integration_v5/MainActivity.java
+++ app/src/main/java/com/iffecode/api_integration_v5/MainActivity.java
@@ -1,9 +1,12 @@
 package com.iffecode.api_integration_v5;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText username;
+    FirebaseAuth auth;
+
+
+    EditText email;
     EditText password;
     Button loginbtn;
     Button registerBtn;
@@ -27,17 +34,49 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        DatabaseReference db = FirebaseDatabase.getInstance().getReference("users");
+        auth = FirebaseAuth.getInstance();
 
-        username = findViewById(R.id.loginUsername);
+        email = findViewById(R.id.loginEmail);
         password = findViewById(R.id.loginPassword);
         loginbtn = findViewById(R.id.loginBtn);
         registerBtn = findViewById(R.id.registerBtn);
+
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                String emailText = email.getText().toString().trim();
+                String passwordText = password.getText().toString().trim();
+
+                if (emailText.isEmpty() || passwordText.isEmpty()){
+
+                    Toast.makeText(MainActivity.this,
+                            "Please fill in email and password",
+                            Toast.LENGTH_SHORT)
+                            .show();
+
+                    return;
+
+                }
+
+                auth.signInWithEmailAndPassword(emailText,passwordText)
+                        .addOnCompleteListener(task -> {
+
+                            if (task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT)
+                                        .show();
+
+                                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                                startActivity(i);
+                                //finish(); //Användaren kan inte trycka bakåt på android knappen och komma tillbaka till login skämen
+
+                            } else {
+                                Toast.makeText(MainActivity.this, "Login failed!",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
 
 
 
@@ -49,6 +88,15 @@ public class MainActivity extends AppCompatActivity {
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                FrameLayout container = findViewById(R.id.registerFragmentContainer);
+                container.setVisibility(View.VISIBLE);
+
+                RegisterFragment fragment = new RegisterFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.registerFragmentContainer, fragment)
+                        .addToBackStack(null)
+                        .commit();
 
             }
         });
